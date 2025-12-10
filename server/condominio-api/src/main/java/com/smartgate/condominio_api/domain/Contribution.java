aceef1d5ebe47974a6121c52f5d35e9ca8ebe4e0
Wallace Gabriel <wgsl@cin.ufpe.br>
2025-12-10 16:09:26 -0300

    hotfix(Model commit)

---
+++ server/condominio-api/src/main/java/com/smartgate/condominio_api/domain/Contribution.java
@@ -0,0 +1,57 @@
+package com.smartgate.condominio_api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Contribution")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Contribution {
+
+    @Id
+    @Column(name = "id_contribution", columnDefinition = "CHAR(36)")
+    private String idContribution;
+
+    // -----------------------------------------------------
+    // RELAÇÃO COM HOUSE
+    // -----------------------------------------------------
+    @ManyToOne(optional = false)
+    @JoinColumn(
+            name = "id_house",
+            referencedColumnName = "id_house",
+            foreignKey = @ForeignKey(name = "fk_contribution_house")
+    )
+    private House house;
+
+    // -----------------------------------------------------
+    // RELAÇÃO COM CONDOMINIUM
+    // -----------------------------------------------------
+    @ManyToOne(optional = false)
+    @JoinColumn(
+            name = "id_condominium",
+            referencedColumnName = "id_condominium",
+            foreignKey = @ForeignKey(name = "fk_contribution_condominium")
+    )
+    private Condominium condominium;
+
+    @Column(name = "payment_date")
+    private LocalDate paymentDate;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
+
+    @PrePersist
+    public void prePersist() {
+        if (idContribution == null) {
+            idContribution = UUID.randomUUID().toString();
+        }
+    }
+}
---
+++ server/condominio-api/src/main/java/com/smartgate/condominio_api/domain/Visitor.java
@@ -0,0 +1,55 @@
+package com.smartgate.condominio_api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Visitor")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Visitor {
+    @Id
+    @Column(name = "id_visitor", columnDefinition = "CHAR(36)")
+    private String idVisitor;
+
+    // -----------------------------
+    //   RELACIONAMENTO COM PERSON
+    // -----------------------------
+    @ManyToOne(optional = false)
+    @JoinColumn(
+            name = "id_person",
+            referencedColumnName = "id_person",
+            foreignKey = @ForeignKey(name = "fk_visitor_person")
+    )
+    private Person person;
+
+    // --------------------------------------
+    //   RELACIONAMENTO COM RESIDENT VISITADO
+    // --------------------------------------
+    @ManyToOne(optional = false)
+    @JoinColumn(
+            name = "id_resident_visited",
+            referencedColumnName = "id_resident",
+            foreignKey = @ForeignKey(name = "fk_visitor_resident")
+    )
+    private Resident residentVisited;
+
+    // -----------------------------
+    //     ENTRY DATETIME
+    // -----------------------------
+    @Column(name = "entry_datetime", insertable = false, updatable = false)
+    private LocalDateTime entryDatetime;
+
+    @PrePersist
+    public void prePersist() {
+        if (idVisitor == null) {
+            idVisitor = UUID.randomUUID().toString();
+        }
+    }
+}
